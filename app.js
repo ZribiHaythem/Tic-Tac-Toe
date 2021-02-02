9bef541170623be4b1f02b84e379e664f5303a83
ZribiHaythem <zribihaythem@gmail.com>
2021-02-02 16:43:57 +0100

    Reload Button added

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 // &#10060
 
-
 var counter = 1;
 
-var play = ['X', 'O']
+var play = ["X", "O"];
 var player1 = play[0];
 var player2 = play[1];
 
 $(".button1").click(function () {
-    counter = 1
+  counter = 1;
 });
 
 $(".button2").click(function () {
-    counter = 2
+  counter = 2;
 });
 
 $("td").on("click", function () {
-    if (!$(this)[0].innerHTML) {
-        if (counter % 2 !== 0) {
-
-            $(this)[0].innerHTML = player1;
-            counter++;
-        }
-        else {
-
-            $(this)[0].innerHTML = player2;
-            counter++;
-        }
+  if (!$(this)[0].innerHTML) {
+    if (counter % 2 !== 0) {
+      $(this)[0].innerHTML = player1;
+      counter++;
+    } else {
+      $(this)[0].innerHTML = player2;
+      counter++;
     }
+  }
 });
 
