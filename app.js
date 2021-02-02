0ec48d417ade27855ad2b4f452abc7dbfd1ec56b
ZribiHaythem <zribihaythem@gmail.com>
2021-02-02 14:24:57 +0100

    yyeeess

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // &#10060
 
 
+var counter = 1;
+var play = ['X','O']
 
 
 $( "td" ).on( "click", function() {
-    $( this )[0].innerHTML = 'X';
+    if(counter % 2 === 0){
+        $( this )[0].innerHTML = play[1];
+        counter ++;
+    } 
+    else {
+        $( this )[0].innerHTML = play[0];
+        counter ++;
+    }
   });
+
+  
