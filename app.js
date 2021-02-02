38654dd82295c9b7a9138c3d3b463b15be142d30
ZribiHaythem <zribihaythem@gmail.com>
2021-02-02 16:13:28 +0100

    fixed not empty square

--- app.js
+++ app.js
@@ -2,18 +2,31 @@
 
 
 var counter = 1;
-var play = ['X','O']
 
+var play = ['X', 'O']
+var player1 = play[0];
+var player2 = play[1];
 
-$( "td" ).on( "click", function() {
-    if(counter % 2 === 0){
-        $( this )[0].innerHTML = play[1];
-        counter ++;
-    } 
-    else {
-        $( this )[0].innerHTML = play[0];
-        counter ++;
+$(".button1").click(function () {
+    counter = 1
+});
+
+$(".button2").click(function () {
+    counter = 2
+});
+
+$("td").on("click", function () {
+    if (!$(this)[0].innerHTML) {
+        if (counter % 2 !== 0) {
+
+            $(this)[0].innerHTML = player1;
+            counter++;
+        }
+        else {
+
+            $(this)[0].innerHTML = player2;
+            counter++;
+        }
     }
-  });
+});
 
-  
